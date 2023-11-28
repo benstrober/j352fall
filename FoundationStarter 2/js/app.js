3522fc6c568408ca36d4ce1e39252448c4da594a
benstrober <144754000+benstrober@users.noreply.github.com>
2023-11-28 10:23:33 -0500

    Add files via upload

---
+++ FoundationStarter 2/js/app.js
@@ -0,0 +1,48 @@
+// Initialize Foundation
+$(document).foundation();
+
+// Set initial slide index
+let slideIndex = 1;
+
+// Show the initial set of slides
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  // Ensure the index is within bounds
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  // Hide all slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  // Remove "active" class from all dots
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  // Display the current slide
+  slides[slideIndex - 1].style.display = "block";
+
+  // Add "active" class to the corresponding dot
+  dots[slideIndex - 1].className += " active";
+}
